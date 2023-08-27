8390aeec3524ceed81d67be1940e4aa1c217c6f3
Matthew Turner <turnmas35@gmail.com>
2023-08-27 01:55:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Matthew is the champion";
+
+document.body.appendChild(newHeader);
